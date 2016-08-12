b847e9e02662b72ab2f11dddc8979eadfeeb220d
alf <alf@alf.com>
2016-08-12 19:26:59 +0200

    xxx

--- .dreamfactory.php
+++ .dreamfactory.php
@@ -2,7 +2,10 @@
 /**
  * This file is part of the DreamFactory Services Platform(tm) Common Components For PHP
  *
- * Copyright (c) 2012-2014 DreamFactory Software, Inc. <support@dreamfactory.com>
+ * DreamFactory Services Platform(tm) <http://github.com/dreamfactorysoftware/dsp-core>
+ * DreamFactory Common Components for PHP <http://github.com/dreamfactorysoftware/lib-php-common>
+ *
+ * Copyright 2012-2014 DreamFactory Software, Inc. <support@dreamfactory.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +18,4 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @copyright 2012-2014 DreamFactory Software, Inc.
- * @license   Apache-2.0 http://opensource.org/licenses/Apache-2.0
- * @version   git: $vector$
  */
-namespace DreamFactory\Common\Interfaces;
-
-/**
- * ServiceProviderLike
- * Acts like something that provides a service
- */
-interface ServiceProviderLike
-{
-}
